d155f9c5da04c142cdcbed38e3e78901672a5a7d
Paul Rozhkin <blackiiifox@gmail.com>
2020-05-23 19:04:27 +0300

    Add tamagotchi remote library

--- app/src/main/java/com/example/restaurantemployerapplication/MainActivity.java
+++ app/src/main/java/com/example/restaurantemployerapplication/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.restaurantemployerapplication;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.tamagotchi.tamagotchiserverprotocol.RestaurantClient;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RestaurantClient.getInstance().getRestaurantsService().getAllRestaurants()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(data -> {
+                    Toast.makeText(this, data.get(0).getAddress(), Toast.LENGTH_LONG).show();
+                }, error -> {
+                    Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show();
+                });
     }
 }
